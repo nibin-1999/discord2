dc70a7d692938eabb4e7faf727dc5c64f65a4a62
nibin-1999 <nibinrp99@gmail.com>
2024-03-07 12:31:06 +0530

    first commit

---
+++ js/style.js
@@ -0,0 +1,36 @@
+function toggleMenu() {
+    console.log('Toggle menu function executed');
+
+    var menu = document.querySelector('.header ul.menu');
+    var body = document.body;
+
+    menu.classList.toggle('is-active');
+    body.classList.toggle('menu-active');
+
+    var logo = document.querySelector('.header h1.logo');
+    logo.classList.toggle('is-active');
+
+    var bar = document.querySelector('.header div.right .toggle-logo .bar');
+    var close = document.querySelector('.header div.right .toggle-logo .close');
+
+    var whiteImg = document.querySelector('.header img.white');
+    var blackImg = document.querySelector('.header img.black');
+
+    if (menu.classList.contains('is-active')) {
+        bar.style.display = 'none';
+        close.style.display = 'block';
+        body.style.overflow = 'hidden';
+
+        // Hide white image and show black image
+        whiteImg.style.display = 'none';
+        blackImg.style.display = 'block';
+    } else {
+        bar.style.display = 'block';
+        close.style.display = 'none';
+        body.style.overflow = 'auto';
+
+        // Hide black image and show white image
+        whiteImg.style.display = 'block';
+        blackImg.style.display = 'none';
+    }
+}
